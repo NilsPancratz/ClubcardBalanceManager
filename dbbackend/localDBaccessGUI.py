b74c0a26dd7e4b06e7a0ad5b13271f341d03fe3c
Nils Pancratz <50653362+NilsPancratz@users.noreply.github.com>
2021-06-19 14:25:06 +0200

    updated numpad etc.

--- dbbackend/localDBaccessGUI.py
+++ dbbackend/localDBaccessGUI.py
@@ -37,10 +37,10 @@ class LocalBalanceManager:
 		self.bottomframe.place(x=0, y=290, relwidth=1, height=400)
 
 
-		self.clubcardInputField = tk.Text(self.topframe, wrap='none', font=("Arial", 27))
+		self.clubcardInputField = tk.Text(self.topframe, wrap='none', font=("Arial", 22))
 		self.clubcardInputField.place(x=10, y=10, width=290, height = 50)
 		
-		self.clubcardInputFieldReset = tk.Button(self.topframe, anchor='c', text='\u2327', bg='#ff9d00', fg='black', font=("Arial", 20), command=lambda: self.clubcardfieldreset())
+		self.clubcardInputFieldReset = tk.Button(self.topframe, anchor='c', text='\u2327', bg='#ff9d00', fg='black', font=("Arial", 20), highlightthickness = 0, bd = 0, command=lambda: self.clubcardfieldreset())
 		self.clubcardInputFieldReset.place(x=310, y=10, width=30, height=50)
 
 		self.clubcardInputField.bind("<Key>", self.check_key)
@@ -48,63 +48,63 @@ class LocalBalanceManager:
 		self.clubcardInputField.focus_set()
 
 
-		self.descriptionLabel = tk.Label(self.topframe, anchor='sw', text="Guthaben:", bg='#ff9d00', fg='black', font=("Arial", 14))
+		self.descriptionLabel = tk.Label(self.topframe, anchor='sw', text="Guthaben:", bg='#ff9d00', fg='black', font=("Arial", 10))
 		self.descriptionLabel.place(x=10, y=64, width=80, height=60)
 
 		self.guthabenLabel = tk.Label(self.topframe, anchor='se', text="keine ID", bg='#ff9d00', fg='black', font=("Arial bold", 44))
 		self.guthabenLabel.place(x=90, y=70, width=250, height=60)
 
 
-		self.button_aufladen = tk.Button(self.topframe, anchor='w', text='aufladen (+)', bg='#ff9d00', fg='#3d9a40', font=("Arial", 20), command=lambda: aufladen(self.currentUID, float(amount_string)))
-		self.button_aufladen.place(x=10, y=150, width=150)
+		self.button_aufladen = tk.Button(self.topframe, anchor='w', text='aufladen (+)', bg='#ff9d00', fg='#3d9a40', font=("Arial", 20), highlightthickness = 0, bd = 0, command=lambda: aufladen(self.currentUID, float(amount_string)))
+		self.button_aufladen.place(x=10, y=130, width=150)
 
-		self.button_belasten = tk.Button(self.topframe, anchor='e', text='belasten (-)', bg='#ff9d00', fg='#b43300', font=("Arial", 20), command=lambda: belasten(self.currentUID, -float(amount_string)))
-		self.button_belasten.place(x=190,y=150, width=150)
+		self.button_belasten = tk.Button(self.topframe, anchor='e', text='belasten (-)', bg='#ff9d00', fg='#b43300', font=("Arial", 20), highlightthickness = 0, bd = 0, command=lambda: belasten(self.currentUID, -float(amount_string)))
+		self.button_belasten.place(x=190,y=130, width=150)
 
 
 		self.transaktionLabel = tk.Label(self.centerframe, anchor='se', text="0.00", bg='black', fg='white', font=("Arial bold", 44))
 		self.transaktionLabel.place(x=90, y=20, width=250, height=60)
 
-		self.cleartransactionbutton = tk.Button(self.centerframe, anchor='c', text="\u2327", bg='black', fg='white', font=("Arial", 20), command=lambda: self.numpad_click_ac())
+		self.cleartransactionbutton = tk.Button(self.centerframe, anchor='c', text="AC", bg='black', fg='white', font=("Arial", 20), highlightthickness = 0, bd = 0, command=lambda: self.numpad_click_ac())
 		self.cleartransactionbutton.place(x=0, y=0, relwidth=1/3, height=100)
 
 
 
 
-		self.button_9 = tk.Button(self.bottomframe, anchor='c', text="9", bg = '#555555', fg='white', font=("Arial", 30), command=lambda: numpad_click_number(9))
+		self.button_9 = tk.Button(self.bottomframe, anchor='c', text="9", bg = '#555555', fg='white', font=("Arial", 30), highlightthickness = 0, bd = 0, command=lambda: numpad_click_number(9))
 		self.button_9.place(x=2*350/3, y=0*400/4, relwidth=1/3, relheight=1/4)
 
-		self.button_8 = tk.Button(self.bottomframe, anchor='c', text="8", bg = '#555555', fg='white', font=("Arial", 30), command=lambda: numpad_click_number(8))
+		self.button_8 = tk.Button(self.bottomframe, anchor='c', text="8", bg = '#555555', fg='white', font=("Arial", 30), highlightthickness = 0, bd = 0, command=lambda: numpad_click_number(8))
 		self.button_8.place(x=1*350/3, y=0*400/4, relwidth=1/3, relheight=1/4)
 
-		self.button_7 = tk.Button(self.bottomframe, anchor='c', text="7", bg = '#555555', fg='white', font=("Arial", 30), command=lambda: numpad_click_number(7))
+		self.button_7 = tk.Button(self.bottomframe, anchor='c', text="7", bg = '#555555', fg='white', font=("Arial", 30), highlightthickness = 0, bd = 0, command=lambda: numpad_click_number(7))
 		self.button_7.place(x=0*350/3, y=0*400/4, relwidth=1/3, relheight=1/4)
 
-		self.button_6 = tk.Button(self.bottomframe, anchor='c', text="6", bg = '#555555', fg='white', font=("Arial", 30), command=lambda: numpad_click_number(6))
+		self.button_6 = tk.Button(self.bottomframe, anchor='c', text="6", bg = '#555555', fg='white', font=("Arial", 30), highlightthickness = 0, bd = 0, command=lambda: numpad_click_number(6))
 		self.button_6.place(x=2*350/3, y=1*400/4, relwidth=1/3, relheight=1/4)
 
-		self.button_5 = tk.Button(self.bottomframe, anchor='c', text="5", bg = '#555555', fg='white', font=("Arial", 30), command=lambda: numpad_click_number(5))
+		self.button_5 = tk.Button(self.bottomframe, anchor='c', text="5", bg = '#555555', fg='white', font=("Arial", 30), highlightthickness = 0, bd = 0, command=lambda: numpad_click_number(5))
 		self.button_5.place(x=1*350/3, y=1*400/4, relwidth=1/3, relheight=1/4)
 
-		self.button_4 = tk.Button(self.bottomframe, anchor='c', text="4", bg = '#555555', fg='white', font=("Arial", 30), command=lambda: numpad_click_number(4))
+		self.button_4 = tk.Button(self.bottomframe, anchor='c', text="4", bg = '#555555', fg='white', font=("Arial", 30), highlightthickness = 0, bd = 0, command=lambda: numpad_click_number(4))
 		self.button_4.place(x=0*350/3, y=1*400/4, relwidth=1/3, relheight=1/4)
 
-		self.button_3 = tk.Button(self.bottomframe, anchor='c', text="3", bg = '#555555', fg='white', font=("Arial", 30), command=lambda: numpad_click_number(3))
+		self.button_3 = tk.Button(self.bottomframe, anchor='c', text="3", bg = '#555555', fg='white', font=("Arial", 30), highlightthickness = 0, bd = 0, command=lambda: numpad_click_number(3))
 		self.button_3.place(x=2*350/3, y=2*400/4, relwidth=1/3, relheight=1/4)
 
-		self.button_2 = tk.Button(self.bottomframe, anchor='c', text="2", bg = '#555555', fg='white', font=("Arial", 30), command=lambda: numpad_click_number(2))
+		self.button_2 = tk.Button(self.bottomframe, anchor='c', text="2", bg = '#555555', fg='white', font=("Arial", 30), highlightthickness = 0, bd = 0, command=lambda: numpad_click_number(2))
 		self.button_2.place(x=1*350/3, y=2*400/4, relwidth=1/3, relheight=1/4)
 
-		self.button_1 = tk.Button(self.bottomframe, anchor='c', text="1", bg = '#555555', fg='white', font=("Arial", 30), command=lambda: numpad_click_number(1))
+		self.button_1 = tk.Button(self.bottomframe, anchor='c', text="1", bg = '#555555', fg='white', font=("Arial", 30), highlightthickness = 0, bd = 0, command=lambda: numpad_click_number(1))
 		self.button_1.place(x=0*350/3, y=2*400/4, relwidth=1/3, relheight=1/4)
 
-		self.button_delete = tk.Button(self.bottomframe, anchor='c', text="\u232b", bg = '#555555', fg='white', font=("Arial", 30), command=lambda: self.numpad_click_delete())
+		self.button_delete = tk.Button(self.bottomframe, anchor='c', text="\u232b", bg = '#555555', fg='white', font=("Arial", 30), highlightthickness = 0, bd = 0, command=lambda: self.numpad_click_delete())
 		self.button_delete.place(x=2*350/3, y=3*400/4, relwidth=1/3, relheight=1/4)
 
-		self.button_dot = tk.Button(self.bottomframe, anchor='c', text=".", bg = '#555555', fg='white', font=("Arial", 30), command=lambda: self.numpad_click_comma())
+		self.button_dot = tk.Button(self.bottomframe, anchor='c', text=".", bg = '#555555', fg='white', font=("Arial", 30), highlightthickness = 0, bd = 0, command=lambda: self.numpad_click_comma())
 		self.button_dot.place(x=1*350/3, y=3*400/4, relwidth=1/3, relheight=1/4)
 
-		self.button_0 = tk.Button(self.bottomframe, anchor='c', text="0", bg = '#555555', fg='white', font=("Arial", 30), command=lambda: numpad_click_number(0))
+		self.button_0 = tk.Button(self.bottomframe, anchor='c', text="0", bg = '#555555', fg='white', font=("Arial", 30), highlightthickness = 0, bd = 0, command=lambda: numpad_click_number(0))
 		self.button_0.place(x=0*350/3, y=3*400/4, relwidth=1/3, relheight=1/4)
 
 
@@ -113,16 +113,19 @@ class LocalBalanceManager:
 		if event.keysym == "Return":
 			text = self.clubcardInputField.get(1.0, tk.END)[:-1]
 			# print(UIDbyteweisetauschen(text))
-			if ":" not in text: #wenn kein : im String ist Wahrscheinlichkeit groß, dass Eingabe ueber RFID Reader erfolgt ist
+			print(len(text))
+			if ":" not in text and len(text)==14: #wenn kein : im String ist und Laenge = 14 Wahrscheinlichkeit groß, dass Eingabe ueber RFID Reader erfolgt ist und Umrechnung erfolgen muss
 				self.clubcardInputField.delete("1.0", "end") #"1.0" und "end" beziehen sich auf das erste Zeichen und das letzte Zeichen des Inhalts 
 				self.clubcardInputField.insert("end", UIDbyteweisetauschen(text))
 				self.currentUID=UIDbyteweisetauschen(text)
 			else:
 				self.currentUID=text
-			print(self.currentUID)
+			# print(self.currentUID)
 			root.focus_set()
 			aktuellesGuthaben=getbalance(self.currentUID)
-			print(aktuellesGuthaben)
+			aktuellesGuthaben_float=float(aktuellesGuthaben)
+			aktuellesGuthaben = "{:.2f}".format(aktuellesGuthaben_float) #string auf zwei Nachkommastellen
+			# print(aktuellesGuthaben)
 			self.guthabenLabel.config(text=aktuellesGuthaben)
 			return "break"
 
@@ -160,7 +163,7 @@ class LocalBalanceManager:
 			amount_string = amount_euros_string + "." + amount_cents_string
 		m.transaktionLabel.config(text=amount_string)
 		currentTransaction=float(amount_string)
-		print(currentTransaction)
+		# print(currentTransaction)
 
 	def numpad_click_ac(self):
 		# print("Numpad_Button AC clicked!")
@@ -174,7 +177,7 @@ class LocalBalanceManager:
 		in_cents = False
 		m.transaktionLabel.config(text=amount_string)
 		currentTransaction=float(amount_string)
-		print(currentTransaction)
+		# print(currentTransaction)
 
 	def clubcardfieldreset(self):
 		self.clubcardInputField.delete("1.0", "end") #"1.0" und "end" beziehen sich auf das erste Zeichen und das letzte Zeichen des Inhalts 
@@ -202,7 +205,7 @@ def numpad_click_number(number_clicked):
 		in_cents = 3
 	m.transaktionLabel.config(text=amount_string)
 	currentTransaction=float(amount_string)
-	print(currentTransaction)
+	# print(currentTransaction)
 
 
 def UIDbyteweisetauschen(data):
@@ -219,23 +222,45 @@ def on_closing():
 		# 	print("")
 		root.destroy()
 
+def on_keypress(event):
+	if "!frame.!text" not in str(event.widget): #nur wenn kein Textfeld ausgewaehlt ist
+		# print(event.char) #welche Taste wurde geklickt?
+		if (event.char in ["0","1","2","3","4","5","6","7","8","9"]): #wenn geklickte Taste eine Nummer ist
+			numpad_click_number(int(event.char))
+		elif event.char in [",","."]:
+			m.numpad_click_comma()
+		elif event.char in ["c","a"]:
+			m.numpad_click_ac()
+		elif event.char == "x":
+			m.clubcardfieldreset()
+		elif event.char == "-":
+			belasten(m.currentUID, -float(amount_string))
+		elif event.char == "+":
+			aufladen(m.currentUID, float(amount_string))
+
+def on_backspace(event): # analog zu on_keypress
+	if "!frame.!text" not in str(event.widget): #nur wenn kein Textfeld ausgewaehlt ist
+		m.numpad_click_delete()
+
 
 def aufladen(UID, amount):
-	if messagebox.askokcancel("Clubkarte aufladen?", "Soll der Clubkarte "+UID+" wirklich " + str(amount)+ " gutgeschrieben werden?"):
-		transferamount(UID, amount)
-		m.guthabenLabel.config(text="keine ID")
-		m.clubcardInputField.delete("1.0", "end") #"1.0" und "end" beziehen sich auf das erste Zeichen und das letzte Zeichen des Inhalts 
-		m.clubcardInputField.focus_set()
-		numpadzuruecksetzen()
+	if m.guthabenLabel.cget("text") != "keine ID" and m.transaktionLabel.cget("text") != "0.00":
+		if messagebox.askokcancel("Clubkarte aufladen?", "Soll der Clubkarte "+UID+" wirklich " + str(amount)+ " gutgeschrieben werden?"):
+			transferamount(UID, amount)
+			m.guthabenLabel.config(text="keine ID")
+			m.clubcardInputField.delete("1.0", "end") #"1.0" und "end" beziehen sich auf das erste Zeichen und das letzte Zeichen des Inhalts 
+			m.clubcardInputField.focus_set()
+			numpadzuruecksetzen()
 
 
 def belasten(UID, amount):
-	if messagebox.askokcancel("Clubkarte aufladen?", "Soll die Clubkarte "+UID+" wirklich mit "+ str(-amount) +" Euro belastet werden?"):
-		transferamount(UID, amount)
-		m.guthabenLabel.config(text="keine ID")
-		m.clubcardInputField.delete("1.0", "end") #"1.0" und "end" beziehen sich auf das erste Zeichen und das letzte Zeichen des Inhalts 
-		m.clubcardInputField.focus_set()
-		numpadzuruecksetzen()
+	if m.guthabenLabel.cget("text") != "keine ID" and m.transaktionLabel.cget("text") != "0.00":
+		if messagebox.askokcancel("Clubkarte aufladen?", "Soll die Clubkarte "+UID+" wirklich mit "+ str(-amount) +" Euro belastet werden?"):
+			transferamount(UID, amount)
+			m.guthabenLabel.config(text="keine ID")
+			m.clubcardInputField.delete("1.0", "end") #"1.0" und "end" beziehen sich auf das erste Zeichen und das letzte Zeichen des Inhalts 
+			m.clubcardInputField.focus_set()
+			numpadzuruecksetzen()
 
 def numpadzuruecksetzen():
 	global amount_euros_string
@@ -248,9 +273,7 @@ def numpadzuruecksetzen():
 	amount_string = amount_euros_string + "." + amount_cents_string
 	in_cents = 0
 	m.transaktionLabel.config(text="0.00")
-
-
-
+ 
 
 # Start der GUI:
 root = tk.Tk()
@@ -259,4 +282,6 @@ root.minsize(windowwidth,windowheight)
 root.maxsize(windowwidth,windowheight)
 m = LocalBalanceManager(root)
 root.protocol("WM_DELETE_WINDOW", on_closing)
+root.bind('<KeyPress>', on_keypress)
+root.bind("<BackSpace>", on_backspace)
 root.mainloop()
